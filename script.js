3ec1867ac267d42058f2e224555b875f9a3d3eae
jen067 <dudu671194@gmail.com>
2024-09-24 07:00:27 +0800

    修改css結構

--- script.js
+++ script.js
@@ -5,9 +5,4 @@ btn.addEventListener("click", () => {
   btn.classList.toggle("active");
   shareBtn.classList.toggle("active");
   links.classList.toggle("appear");
-  if (btn.classList.contains("active")) {
-    btn.style.top = ".5rem";
-  } else {
-    btn.style.top = "0";
-  }
 });
