db3a048245e4c23d53f039c8599f9c50962b4245
jen067 <dudu671194@gmail.com>
2024-09-24 01:07:41 +0800

    完成share位置切換

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ btn.addEventListener("click", () => {
   btn.classList.toggle("active");
   shareBtn.classList.toggle("active");
   links.classList.toggle("appear");
+  if (btn.classList.contains("active")) {
+    btn.style.top = ".5rem";
+  } else {
+    btn.style.top = "0";
+  }
 });
